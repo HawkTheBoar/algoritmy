a5b70eea82dbe4ec49ccd90a8af4b68ea02dcb98
Mistr <michal.stransky42@gmail.com>
2024-09-16 13:29:41 +0200

    spiral :3

---
+++ spiral-001/spiral-loop.py
@@ -0,0 +1,22 @@
+import turtle, tkinter
+from functools import cache
+
+if __name__ == "__main__":
+    print("Zadejte délku stěny spirály: ", end="")
+    L = int(input()) * 10
+    print("Zadejte sířku mezery mezi stěnami spirály: ", end="")
+    W = int(input()) * 10
+
+    screen = turtle.Screen()
+    t = turtle.Turtle()
+
+    turtle.forward(L)
+    turtle.right(90)
+    while(L > W):
+        for i in range(2):
+            turtle.forward(L)
+            turtle.right(90)
+        L -= W
+
+
+    screen.exitonclick()
