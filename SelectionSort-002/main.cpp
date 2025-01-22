5b398658f168340aabea40592ce183f62f593a46
Mistr <michalstransky42@gmail.com>
2025-01-22 20:04:43 +0100

    string sorting (not working)

--- SelectionSort-002/main.cpp
+++ SelectionSort-002/main.cpp
@@ -9,6 +9,23 @@
 #include <cctype>
 using namespace std;
 
+string strToLower(string s){
+    transform(s.begin(), s.end(), s.begin(), [](unsigned char c) {
+         return tolower(c);
+    });
+    return s;
+}
+int stringToInt(const std::string& str) {
+    int value = 0;
+    string lwr = strToLower(str);
+    // Convert the string to a numeric value based on ASCII values
+    for (char c : lwr) {
+        value = value * 31 + c; // 31 is a small prime multiplier to create unique values
+    }
+
+    return value;
+}
+
 void swap(int &a, int &b){
     int temp = a;
     a = b;
@@ -37,6 +54,24 @@ std::vector<int> readNumbersFromFile(const std::string& filename) {
     }
 
     int num;
+    while (inputFile >> num) {
+        numbers.push_back(num);  // Add each number to the vector
+        inputFile.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore the newline character
+    }
+
+    inputFile.close();  // Close the file
+    return numbers;  // Return the vector containing the numbers
+}
+std::vector<string> readWordsFromFile(const std::string& filename) {
+    std::ifstream inputFile(filename);  // Open the text file
+    std::vector<string> numbers;  // Vector to hold numbers
+
+    if (!inputFile) {
+        std::cerr << "Error opening file!" << std::endl;
+        return numbers;  // Return empty vector on failure
+    }
+
+    string num;
     while (inputFile >> num) {
         numbers.push_back(num);  // Add each number to the vector
         inputFile.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore the newline character
@@ -80,23 +115,47 @@ void sort(std::vector<int>* arr){
         }
     }
 }
-void printArr(std::vector<int>* arr){
+void sortStrings(std::vector<string>* arr){
+    int n = arr->size();
+    int outputInterval = 10000;
+    for(int i = 0; i < n - 1; i++){
+        int minIndex = i;
+        for(int j = i + 1; j < n; j++){
+            if(stringToInt((*arr)[j]) < stringToInt((*arr)[minIndex])){
+                minIndex = j;
+            }
+        }
+        swap((*arr)[i], (*arr)[minIndex]);
+        if(i % outputInterval == 0 && i != 0){
+            std::cout << i << "/" << n << "\n";
+        }
+    }
+}void printArr(std::vector<int>* arr){
     for(int i = 0; i < arr->size(); i++){
         std::cout << arr->at(i) << " ";
     }
 }
-string strToLower(string s){
-    transform(s.begin(), s.end(), s.begin(), [](unsigned char c) {
-         return tolower(c);
-    });
-    return s;
+std::string arrToString(std::vector<int>* arr) {
+    std::string res = "";
+    for (int i = 0; i < arr->size(); i++) {
+        res += std::to_string(arr->at(i)) + "\n";  // Convert integer to string and concatenate
+    }
+    return res;
+}
+std::string arrToString(std::vector<string>* arr) {
+    std::string res = "";
+    for (int i = 0; i < arr->size(); i++) {
+        res += arr->at(i) + "\n";  // Convert integer to string and concatenate
+    }
+    return res;
 }
 int main(){
     
     Timer readTimer;
     readTimer.start();
     // std::vector<int> res = readNumbersFromFile("test.txt");
-    std::vector<int> res = readNumbersFromFile("input.txt");
+    // std::vector<int> res = readNumbersFromFile("input.txt");
+    std::vector<string> res = readWordsFromFile("string.txt");
     if(!res.empty()){
         for(auto i = res.begin(); i != res.end(); i++){
         }
@@ -104,11 +163,17 @@ int main(){
     readTimer.stop();
     Timer sortTimer;
     sortTimer.start();
-    sort(&res);
+    // sort(&res);
+    sortStrings(&res);
+    std::string sorted = arrToString(&res);
+
+    sortStrings(&res);
     // printArr(&res);
     sortTimer.stop();
     std::string readFileTime = "Time taken to read the file: " + std::to_string(readTimer.getDuration()) + " seconds.\n";
     std::string sortFileTime = "Time taken to sort the file: " + std::to_string(sortTimer.getDuration()) + " seconds.\n";
     std::string totalTimeTaken = "Total time taken: " + std::to_string(sortTimer.getDuration()+readTimer.getDuration())+ " seconds.\n";
     writeToFile("output.txt", readFileTime + sortFileTime + totalTimeTaken);
+    writeToFile("sortedArr.txt", sorted);
+
 }
